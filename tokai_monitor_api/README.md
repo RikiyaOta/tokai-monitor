9a12dbf0569eb26e9b0189749573692350d48101
rikiyaota <rikiya0421.fcb6@gmail.com>
2021-09-14 19:39:21 +0900

    Remove original tokai_monitor_api.

--- tokai_monitor_api/README.md
+++
@@ -1,7 +0,0 @@
-# TokaiMonitorAPI
-
-## Versions
-
-- Elixir: 1.12.3
-- Erlang: 24.0.6
-- PostgreSQL: 13.4
--- tokai_monitor_api/lib/tokai_monitor_api/helpers/map_helper.ex
+++
@@ -1,8 +0,0 @@
-defmodule TokaiMonitorAPI.Helper.MapHelper do
-  def delete_nil_keys(map) do
-    Enum.reduce(map, %{}, fn
-      {_key, nil}, acc -> acc
-      {key, value}, acc -> Map.put(acc, key, value)
-    end)
-  end
-end
