1623e32f2e4d6a24c4f480ec2351069398f16ea7
rikiyaota <rikiya0421.fcb6@gmail.com>
2021-09-18 19:16:17 +0900

    Implement a base card list.

--- tokai_monitor_frontend/src/App.js
+++ tokai_monitor_frontend/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import AppBar from '@mui/material/AppBar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            東海モニター
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card sx={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image="https://yt3.ggpht.com/ytc/AKedOLTDLauymQQXmfG3S_r3ZTzw8ds1VstwhcwvHU_8OA=s88-c-k-c0x00ffffff-no-rj"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      東海オンエア
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
   );
 }
-
-export default App;
