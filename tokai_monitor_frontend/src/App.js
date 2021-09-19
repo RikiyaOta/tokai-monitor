6ad191966d5ddc92e52e6d515b0f49262ce8be70
rikiyaota <rikiya0421.fcb6@gmail.com>
2021-09-19 11:02:34 +0900

    Implement the base ranking page.

--- tokai_monitor_frontend/src/App.js
+++ tokai_monitor_frontend/src/App.js
@@ -1,73 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import _ from 'lodash';
-
-import './App.css';
-
-import AppBar from '@mui/material/AppBar';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-import { fetchChannels } from './helpers/api.js';
-
-const theme = createTheme();
+import React from 'react';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import Ranking from './Ranking';
 
 export default function App() {
-  const [channels, setChannels] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const result = await fetchChannels();
-      const fetchedChannels = result.data.channels;
-      setChannels(_.sortBy(fetchedChannels, ['index_number']));
-    }
-    fetchData();
-  }, []);
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            東海モニター
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={4}>
-            {
-              _.map(channels, channel => {
-                return (
-                  <Grid key={channel.id} item xs={12} sm={6} md={4}>
-                    <Card sx={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          image={channel.thumbnail_url}
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                            {channel.title}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                );
-              })
-            }
-          </Grid>
-        </Container>
-      </main>
-    </ThemeProvider>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/channels/:channel_id/ranking">
+          <Ranking />
+        </Route>
+      </Switch>
+    </>
   );
 }
---
+++ tokai_monitor_frontend/src/Home.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
+
+import './App.css';
+
+import AppBar from '@mui/material/AppBar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { fetchChannels } from './helpers/api.js';
+
+const theme = createTheme();
+
+
+export default function Home() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await fetchChannels();
+      const fetchedChannels = result.data.channels;
+      setChannels(_.sortBy(fetchedChannels, ['index_number']));
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            東海モニター
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Grid container spacing={4}>
+            {
+              _.map(channels, channel => {
+                return (
+                  <Grid key={channel.id} item xs={12} sm={6} md={4}>
+                    <Link to={`/channels/${channel.id}/ranking`} style={{"textDecoration": "none"}}>
+                      <Card sx={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+                        <CardActionArea>
+                          <CardMedia
+                            component="img"
+                            image={channel.thumbnail_url}
+                          />
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                              {channel.title}
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Link>
+                  </Grid>
+                );
+              })
+            }
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
+}
